56a5f9868707b9d72bc277884d5b6320ecd40b43
Joaquin-DG <fuencalientejoaquin@gmail.com>
2026-09-04 23:56:07 +0200

    C++ conversion

--- main/nfc.cpp
+++ main/nfc.cpp
@@ -1,15 +1,18 @@
-#include <stdio.h>
+#include <iostream>
 #include "freertos/FreeRTOS.h"             // Núcleo de FreeRTOS para tipos básicos
 #include "freertos/task.h"                 // Gestión de tareas y retrasos bloqueantes
+#include "pn532.h"
 #include "esp_log.h"                       // Biblioteca nativa de logs de ESP-IDF
 
-void app_main(void)
+extern "C" void app_main(void)
 {
     // ESP_LOGI("Inicio programa");
 
+    
+
     while (1) {
 
-        printf("Buscando a nemo\n");
+        std::cout << "Diselo mike" << std::endl;
 
         // Retardo pasivo de 2 segundos para no saturar el procesador
         vTaskDelay(pdMS_TO_TICKS(2000));
